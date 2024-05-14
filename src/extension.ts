3474b83c7c22772928c7fdd8be0b91de1284af3d
kintsoogi <kintsoogii@proton.me>
2024-05-14 16:24:56 -0500

    Added story outline webview sidebar

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 import { ExtensionContext } from "vscode";
 import { ObsEditorProvider } from "./providers/ObsEditorProvider";
+import { StoryOutlineProvider } from "./providers/storyOutlineProvider";
 
 export function activate(context: ExtensionContext) {
   context.subscriptions.push(ObsEditorProvider.register(context));
+  context.subscriptions.push(StoryOutlineProvider.register(context));
 }
